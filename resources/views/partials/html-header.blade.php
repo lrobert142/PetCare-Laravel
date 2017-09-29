9cd60c9ac54f699d76589ed3fe2d8cafbed1ef11
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-09-29 17:30:58 +1000

    SASS setup and create pathway

--- resources/views/partials/html-header.blade.php
+++ resources/views/partials/html-header.blade.php
@@ -2,24 +2,5 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pet Care App</title>
-  <!-- TODO Move styles to CSS file. Compile CSS file from SASS -->
-  <style>
-    .pet__card {
-      display: flex;
-      border: thin solid grey;
-      border-radius: 10px;
-      padding: 20px;
-    }
-
-    .pet__card--image {
-      display: inline-block;
-      width: auto;
-      height: 100%;
-      margin-right: 20px;
-    }
-
-    .pet__card--details {
-      display: inline-block;
-    }
-  </style>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -1,25 +1,35 @@
 @extends('base')
 
 @section('content')
+  @if ($errors->any())
+    <div class="errors__container">
+      <ul class="errors">
+        @foreach ($errors->all() as $error)
+          <li class="error">{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
   <form method="POST" action="/pets" enctype="multipart/form-data">
     <label for="name">Name:</label>
-    <input id="name" type="text" name="name" placeholder="Name" required />
+    <input id="name" type="text" name="name" placeholder="Name"  />
     <br />
 
     <label for="date_of_birth">Date of Birth:</label>
-    <input id="date_of_birth" type="date" name="date_of_birth" placeholder="YYYY-MM-DD" required />
+    <input id="date_of_birth" type="date" name="date_of_birth" placeholder="YYYY-MM-DD"  />
     <br />
 
     <label for="weight">Weight (g):</label>
-    <input id="weight" type="number" name="weight" placeholder="Weight in grams" required />
+    <input id="weight" type="number" name="weight" placeholder="Weight in grams"  />
     <br />
 
     <label for="photo">Photo</label>
-    <input id="photo" type="file" name="photo" placeholder="Photo" required />
+    <input id="photo" type="file" name="photo" placeholder="Photo"  />
     <br />
 
     {{ csrf_field() }}
 
-    <input type="submit" value="Submit" />
+    <input class="button--primary" type="submit" value="Submit" />
   </form>
 @endsection

093a22a6767efdd0f06d6aa46d10538c9952fbda
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-09-29 17:41:53 +1000

    Remodal and removal of bootstrap items

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
   <h1>Pets</h1>
+  <a href="#add-pet">Add New</a>
   @if(count($pets) > 0)
     @foreach($pets as $pet)
       <div class="pet__card">
@@ -21,8 +22,45 @@
     @endforeach
   @else
     <p>
-      You do not yet have any pets added. Why not <a href="#">add one now!</a>
+      You do not yet have any pets added. Why not <a href="#add-pet">add one now!</a>
     </p>
   @endif
+
+  <div class="remodal" data-remodal-id="add-pet">
+    <button data-remodal-action="close" class="remodal-close"></button>
+    <h1>New Pet</h1>
+
+    @if ($errors->any())
+      <div class="errors__container">
+        <ul class="errors">
+          @foreach ($errors->all() as $error)
+            <li class="error">{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
+    <form method="POST" action="/pets" enctype="multipart/form-data">
+      <label for="name">Name:</label>
+      <input id="name" type="text" name="name" placeholder="Name"  />
+      <br />
+
+      <label for="date_of_birth">Date of Birth:</label>
+      <input id="date_of_birth" type="date" name="date_of_birth" placeholder="YYYY-MM-DD"  />
+      <br />
+
+      <label for="weight">Weight (g):</label>
+      <input id="weight" type="number" name="weight" placeholder="Weight in grams"  />
+      <br />
+
+      <label for="photo">Photo</label>
+      <input id="photo" type="file" name="photo" placeholder="Photo"  />
+      <br />
+
+      {{ csrf_field() }}
+
+      <input class="button--primary" type="submit" value="Submit" />
+    </form>
+  </div>
 
 @endsection

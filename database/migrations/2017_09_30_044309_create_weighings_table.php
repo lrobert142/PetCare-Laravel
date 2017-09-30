30e2c84e02a722e9fd6e8dcb344e86b701d1ad6c
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-09-30 14:47:24 +1000

    Weighings migration

---
+++ database/migrations/2017_09_30_044309_create_weighings_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeighingsTable extends Migration
+{
+    public function up()
+    {
+      Schema::create('weighings', function (Blueprint $table) {
+        $table->increments('id');
+        $table->integer('pet_id');
+        $table->integer('weight');
+        $table->text('notes')->nullable();
+        $table->timestamps();
+        $table->softDeletes();
+
+        $table->foreign('pet_id')->references('id')->on('pets');
+      });
+    }
+
+    public function down()
+    {
+      Schema::dropIfExists('weighings');
+    }
+}
